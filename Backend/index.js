4cc0019d135756a804ebaec296d4a3bc6229e42e
Naimul Islam <nayemhasan1314@gmail.com>
2025-08-18 16:14:34 +0600

    changed cors origin

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ import { verifyToken } from "./middlewares/authMiddleware.js";
 
 const app = express();
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: `${process.env.CROSS_ORIGIN}` || "http://localhost:5173",
     credentials: true
 }));
 app.use(express.json());
